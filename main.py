d17a64ce8e7a0df985028df39b1a293a7884a5eb
andreac0ntreras <andrea.contreras.fernandez90@gmail.com>
2023-03-07 16:20:04 -0500

    added main code

---
+++ main.py
@@ -0,0 +1,44 @@
+# Lab 6 - GitHub lab
+
+def encode(password):
+    encoded = ""
+    for i in password:
+        digit = int(i) + 3
+        encoded += str(digit)
+    return encoded
+
+
+def decode(password):
+    decoded = ""
+    for i in password:
+        digit = int(i) - 3
+        decoded += str(digit)
+    return decoded
+
+
+def main():
+
+    encoded = None
+
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        print()
+
+        menu_selection = int(input("Please enter an option: "))
+        if menu_selection == 1:
+            password = input("Please enter your password to encode: ")
+            encoded = encode(password)
+
+        elif menu_selection == 2:
+            decoded = decode(encoded)
+            print("The encoded password is " + encoded + " and the original password is " + decoded + ".")
+
+        elif menu_selection == 3:
+            break
+
+if __name__ == "__main__":
+    main()
